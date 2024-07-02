95ad60fad113352e5ee9dece649d15690e5c4a6b
Raluuxi <raluuxi@yahoo.com>
2024-07-02 15:33:30 +0300

    refacut

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,28 +24,28 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.BtnIncarca = New System.Windows.Forms.Button()
         Me.BtnSalveaza = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TxtPrenume = New System.Windows.Forms.TextBox()
         Me.txtNume = New System.Windows.Forms.TextBox()
         Me.txtId = New System.Windows.Forms.TextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dgv = New System.Windows.Forms.DataGridView()
         Me.BtnSterge = New System.Windows.Forms.Button()
         Me.BtnModifica = New System.Windows.Forms.Button()
         Me.TxtFiltreaza = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.BtnSorteaza = New System.Windows.Forms.Button()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'BtnIncarca
         '
-        Me.BtnIncarca.Location = New System.Drawing.Point(394, 253)
+        Me.BtnIncarca.Location = New System.Drawing.Point(394, 202)
         Me.BtnIncarca.Name = "BtnIncarca"
         Me.BtnIncarca.Size = New System.Drawing.Size(142, 27)
-        Me.BtnIncarca.TabIndex = 27
+        Me.BtnIncarca.TabIndex = 3
         Me.BtnIncarca.Text = "Incarca date"
         Me.BtnIncarca.UseVisualStyleBackColor = True
         '
@@ -54,18 +54,18 @@ Partial Class Form1
         Me.BtnSalveaza.Location = New System.Drawing.Point(394, 302)
         Me.BtnSalveaza.Name = "BtnSalveaza"
         Me.BtnSalveaza.Size = New System.Drawing.Size(142, 27)
-        Me.BtnSalveaza.TabIndex = 26
+        Me.BtnSalveaza.TabIndex = 5
         Me.BtnSalveaza.Text = "Salveaza"
         Me.BtnSalveaza.UseVisualStyleBackColor = True
         '
-        'Button1
+        'BtnAdd
         '
-        Me.Button1.Location = New System.Drawing.Point(394, 202)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(142, 27)
-        Me.Button1.TabIndex = 25
-        Me.Button1.Text = "Adauga"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BtnAdd.Location = New System.Drawing.Point(394, 253)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(142, 27)
+        Me.BtnAdd.TabIndex = 4
+        Me.BtnAdd.Text = "Adauga"
+        Me.BtnAdd.UseVisualStyleBackColor = True
         '
         'Label3
         '
@@ -73,7 +73,7 @@ Partial Class Form1
         Me.Label3.Location = New System.Drawing.Point(38, 101)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(49, 13)
-        Me.Label3.TabIndex = 24
+        Me.Label3.TabIndex = 12
         Me.Label3.Text = "Prenume"
         '
         'Label2
@@ -82,7 +82,7 @@ Partial Class Form1
         Me.Label2.Location = New System.Drawing.Point(38, 64)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(35, 13)
-        Me.Label2.TabIndex = 23
+        Me.Label2.TabIndex = 11
         Me.Label2.Text = "Nume"
         '
         'Label1
@@ -91,7 +91,7 @@ Partial Class Form1
         Me.Label1.Location = New System.Drawing.Point(38, 26)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(16, 13)
-        Me.Label1.TabIndex = 22
+        Me.Label1.TabIndex = 10
         Me.Label1.Text = "Id"
         '
         'TxtPrenume
@@ -99,36 +99,36 @@ Partial Class Form1
         Me.TxtPrenume.Location = New System.Drawing.Point(94, 101)
         Me.TxtPrenume.Name = "TxtPrenume"
         Me.TxtPrenume.Size = New System.Drawing.Size(129, 20)
-        Me.TxtPrenume.TabIndex = 21
+        Me.TxtPrenume.TabIndex = 2
         '
         'txtNume
         '
         Me.txtNume.Location = New System.Drawing.Point(94, 61)
         Me.txtNume.Name = "txtNume"
         Me.txtNume.Size = New System.Drawing.Size(129, 20)
-        Me.txtNume.TabIndex = 20
+        Me.txtNume.TabIndex = 1
         '
         'txtId
         '
         Me.txtId.Location = New System.Drawing.Point(94, 23)
         Me.txtId.Name = "txtId"
         Me.txtId.Size = New System.Drawing.Size(129, 20)
-        Me.txtId.TabIndex = 19
+        Me.txtId.TabIndex = 0
         '
-        'DataGridView1
+        'dgv
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(36, 142)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(341, 242)
-        Me.DataGridView1.TabIndex = 18
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(36, 142)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(341, 242)
+        Me.dgv.TabIndex = 13
         '
         'BtnSterge
         '
         Me.BtnSterge.Location = New System.Drawing.Point(563, 202)
         Me.BtnSterge.Name = "BtnSterge"
         Me.BtnSterge.Size = New System.Drawing.Size(142, 27)
-        Me.BtnSterge.TabIndex = 28
+        Me.BtnSterge.TabIndex = 6
         Me.BtnSterge.Text = "Sterge"
         Me.BtnSterge.UseVisualStyleBackColor = True
         '
@@ -137,7 +137,7 @@ Partial Class Form1
         Me.BtnModifica.Location = New System.Drawing.Point(563, 253)
         Me.BtnModifica.Name = "BtnModifica"
         Me.BtnModifica.Size = New System.Drawing.Size(142, 27)
-        Me.BtnModifica.TabIndex = 29
+        Me.BtnModifica.TabIndex = 7
         Me.BtnModifica.Text = "Modifica"
         Me.BtnModifica.UseVisualStyleBackColor = True
         '
@@ -146,7 +146,7 @@ Partial Class Form1
         Me.TxtFiltreaza.Location = New System.Drawing.Point(150, 399)
         Me.TxtFiltreaza.Name = "TxtFiltreaza"
         Me.TxtFiltreaza.Size = New System.Drawing.Size(171, 20)
-        Me.TxtFiltreaza.TabIndex = 30
+        Me.TxtFiltreaza.TabIndex = 9
         '
         'Label4
         '
@@ -154,7 +154,7 @@ Partial Class Form1
         Me.Label4.Location = New System.Drawing.Point(70, 402)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(76, 13)
-        Me.Label4.TabIndex = 31
+        Me.Label4.TabIndex = 14
         Me.Label4.Text = "Filtreaza dupa:"
         '
         'BtnSorteaza
@@ -162,7 +162,7 @@ Partial Class Form1
         Me.BtnSorteaza.Location = New System.Drawing.Point(563, 305)
         Me.BtnSorteaza.Name = "BtnSorteaza"
         Me.BtnSorteaza.Size = New System.Drawing.Size(142, 23)
-        Me.BtnSorteaza.TabIndex = 32
+        Me.BtnSorteaza.TabIndex = 8
         Me.BtnSorteaza.Text = "Sorteaza"
         Me.BtnSorteaza.UseVisualStyleBackColor = True
         '
@@ -178,17 +178,17 @@ Partial Class Form1
         Me.Controls.Add(Me.BtnSterge)
         Me.Controls.Add(Me.BtnIncarca)
         Me.Controls.Add(Me.BtnSalveaza)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.BtnAdd)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TxtPrenume)
         Me.Controls.Add(Me.txtNume)
         Me.Controls.Add(Me.txtId)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dgv)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -196,14 +196,14 @@ Partial Class Form1
 
     Friend WithEvents BtnIncarca As Button
     Friend WithEvents BtnSalveaza As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents BtnAdd As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents TxtPrenume As TextBox
     Friend WithEvents txtNume As TextBox
     Friend WithEvents txtId As TextBox
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dgv As DataGridView
     Friend WithEvents BtnSterge As Button
     Friend WithEvents BtnModifica As Button
     Friend WithEvents TxtFiltreaza As TextBox
--- Form1.vb
+++ Form1.vb
@@ -1,70 +1,101 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Dim dt As New DataTable
     Dim dv As New DataView(dt)
+
     Dim numarApasari = 0
     Dim filtru As String
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        dt.Rows.Add(txtId.Text, txtNume.Text, TxtPrenume.Text)
-
-        txtId.Clear()
-        txtNume.Clear()
-        TxtPrenume.Clear()
-
-    End Sub
+    Dim startupPath As String = Application.StartupPath
+    Dim filePath As String = Path.Combine(startupPath, "detalii.xml")
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
 
-        dt.Columns.Add("Id", Type.GetType("System.Int32"))
-        dt.Columns.Add("Nume", Type.GetType("System.String"))
-        dt.Columns.Add("Prenume", Type.GetType("System.String"))
+        If File.Exists(filePath) Then
 
-        dt.Rows.Add("1", "Ion", "Alexandra")
-        dt.Rows.Add("2", "Ionascu", "Iulia")
-        dt.Rows.Add("3", "Ciobotaru", "Narcis")
-        dt.Rows.Add("4", "Constantin", "Paul")
-        dt.Rows.Add("5", "Silion", "Gabriel")
-        dt.Rows.Add("6", "Pruteanu", "Alexandru")
-        dt.Rows.Add("7", "Serban", "Christin")
-        dt.Rows.Add("8", "Mehedinti", "Oana")
-        dt.Rows.Add("9", "Nanau", "Georgiana")
-        dt.Rows.Add("10", "Neculache", "Maria")
+            MsgBox("Fisierul a fost gasit la: " & filePath)
 
-        DataGridView1.DataSource = dt
-        dt.TableName = "Detalii"
+            dt.ReadXml(Application.StartupPath & "detalii.xml")
+
+        Else
+            MsgBox("Fisierul nu a fost gasit")
+
+            dt.Columns.Add("Id", Type.GetType("System.Int32"))
+            dt.Columns.Add("Nume", Type.GetType("System.String"))
+            dt.Columns.Add("Prenume", Type.GetType("System.String"))
+
+            dt.Rows.Add("1", "Ion", "Alexandra")
+            dt.Rows.Add("2", "Ionascu", "Iulia")
+            dt.Rows.Add("3", "Ciobotaru", "Narcis")
+            dt.Rows.Add("4", "Constantin", "Paul")
+            dt.Rows.Add("5", "Silion", "Gabriel")
+            dt.Rows.Add("6", "Pruteanu", "Alexandru")
+            dt.Rows.Add("7", "Serban", "Christin")
+            dt.Rows.Add("8", "Mehedinti", "Oana")
+            dt.Rows.Add("9", "Nanau", "Georgiana")
+            dt.Rows.Add("10", "Neculache", "Maria")
+
+        End If
+
+        dgv.DataSource = dt
+        dgv.DataSource = dv
+
+        'dt.TableName = "Detalii"
 
     End Sub
-    Private Sub BtnIncarca_Click(sender As Object, e As EventArgs) Handles BtnIncarca.Click
-        dt.WriteXml("D:\detalii.xml", XmlWriteMode.WriteSchema)
-    End Sub
-    Private Sub BtnSalveaza_Click(sender As Object, e As EventArgs) Handles BtnSalveaza.Click
-        dt.ReadXml("D:\detalii.xml")
+    Private Sub dgv_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv.CellClick
+
+        txtId.Text = dgv.CurrentRow.Cells(0).Value.ToString
+        txtNume.Text = dgv.CurrentRow.Cells(1).Value.ToString
+        TxtPrenume.Text = dgv.CurrentRow.Cells(2).Value.ToString
+
     End Sub
 
-    Private Sub BtnSterge_Click(sender As Object, e As EventArgs) Handles BtnSterge.Click
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        If Not txtId.Text = "" And txtNume.Text = "" And TxtPrenume.Text = "" Then
 
-        If DataGridView1.CurrentRow.Selected Then
-            DataGridView1.Rows.Remove(DataGridView1.CurrentRow)
-            txtId.Text = Nothing
-            txtNume.Text = Nothing
-            TxtPrenume.Text = Nothing
+            dt.Rows.Add(txtId.Text, txtNume.Text, TxtPrenume.Text)
+
+            txtId.Clear()
+            txtNume.Clear()
+            TxtPrenume.Clear()
+        Else
+            MsgBox("Adauga valori in toate casetele")
         End If
 
     End Sub
+    Private Sub BtnSterge_Click(sender As Object, e As EventArgs) Handles BtnSterge.Click
 
-    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If dgv.CurrentRow.Selected Then
 
-        txtId.Text = DataGridView1.CurrentRow.Cells(0).Value.ToString
-        txtNume.Text = DataGridView1.CurrentRow.Cells(1).Value.ToString
-        TxtPrenume.Text = DataGridView1.CurrentRow.Cells(2).Value.ToString
+            dgv.Rows.Remove(dgv.CurrentRow)
+            txtId.Text = Nothing
+            txtNume.Text = Nothing
+            TxtPrenume.Text = Nothing
+        Else
+            MsgBox("Selecteaza un rand")
+        End If
 
+    End Sub
+    Private Sub BtnIncarca_Click(sender As Object, e As EventArgs) Handles BtnIncarca.Click
+        dt.ReadXml(Application.StartupPath & "detalii.xml")
+    End Sub
+    Private Sub BtnSalveaza_Click(sender As Object, e As EventArgs) Handles BtnSalveaza.Click
+        dt.WriteXml(Application.StartupPath & "detalii.xml", XmlWriteMode.WriteSchema)
     End Sub
 
     Private Sub BtnModifica_Click(sender As Object, e As EventArgs) Handles BtnModifica.Click
 
-        DataGridView1.SelectedRows(0).Cells(0).Value = txtId.Text
-        DataGridView1.SelectedRows(0).Cells(1).Value = txtNume.Text
-        DataGridView1.SelectedRows(0).Cells(2).Value = TxtPrenume.Text
+        If Not dgv.CurrentRow.Selected Then
+            MsgBox("Selecteaza un rand")
+        ElseIf txtId.Text = "" Or txtNume.Text = "" Or TxtPrenume.Text = "" Then
+            MsgBox("Scrie valori in toate casetele")
+        Else
+            dgv.SelectedRows(0).Cells(0).Value = txtId.Text
+            dgv.SelectedRows(0).Cells(1).Value = txtNume.Text
+            dgv.SelectedRows(0).Cells(2).Value = TxtPrenume.Text
+        End If
 
     End Sub
     Private Sub BtnSorteaza_Click(sender As Object, e As EventArgs) Handles BtnSorteaza.Click
@@ -77,16 +108,10 @@
             dv.Sort = "Id ASC"
         End If
 
-        DataGridView1.DataSource = dv
-
     End Sub
     Private Sub TxtFiltreaza_TextChanged(sender As Object, e As EventArgs) Handles TxtFiltreaza.TextChanged
-
         filtru = TxtFiltreaza.Text
         dv.RowFilter = $"Prenume LIKE '%{filtru}%'"
-
-        DataGridView1.DataSource = dv
-
     End Sub
 
 
